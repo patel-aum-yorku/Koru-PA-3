f6342b18bdd2e84597c7450c021085c24907681d
Prabhjee Singh <prabhjeesingh1@gmail.com>
2025-02-11 17:57:36 -0500

    Added Tailwind and updated node_modules

---
+++ koru-pa-3/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      gridColumn: {
+          '70/30': '70% 28%'
+      }
+    },
+  },
+  plugins: [],
+}
+
